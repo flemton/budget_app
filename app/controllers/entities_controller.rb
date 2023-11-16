7351903fea24590e5cd8ebce16f146aeeff7e853
Alhassan Osman <oalhassan847@gmail.com>
2023-11-16 21:48:28 +0000

    Add new and create actions to entities controller

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,7 +1,31 @@
-class GroupsController < ApplicationController
+class EntitiesController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :set_group
+
   def index
-    @groups = current_user.groups
+    @entities = @group.entities
+  end
+
+  def new
+    @entity = @group.entities.build
+  end
+
+  def create
+    @entity = @group.entities.build(entity_params.merge(user: current_user))
+    if @entity.save
+      redirect_to group_entities_path(@group)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_group
+    @group = current_user.groups.find(params[:group_id])
+  end
+
+  def entity_params
+    params.require(:entity).permit(:name, :amount)
   end
 end
