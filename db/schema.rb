05f65f417ca183048eae33b2852dc9840c4e6b3a
Alhassan Osman <oalhassan847@gmail.com>
2023-11-16 22:07:30 +0000

    Add and run user-group association migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_16_203845) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_16_220540) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_16_203845) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -47,4 +49,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_16_203845) do
 
   add_foreign_key "entities", "groups"
   add_foreign_key "entities", "users"
+  add_foreign_key "groups", "users"
 end
