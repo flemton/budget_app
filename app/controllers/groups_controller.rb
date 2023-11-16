7218c5c0b7b4f487ff31690a1191004cb61dfd17
Alhassan Osman <oalhassan847@gmail.com>
2023-11-16 21:07:37 +0000

    Add index action for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,7 @@
-class GroupsController < ApplicationController
+class EntitiesController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @entities = current_user.entities
+  end
 end
