7563c756d2a76421bf1113b6364a85b923fd7ade
Alhassan Osman <oalhassan847@gmail.com>
2023-11-16 21:48:02 +0000

    Add new and create actions to groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,26 @@
-class EntitiesController < ApplicationController
+class GroupsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
-    @entities = current_user.entities
+    @groups = current_user.groups
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = current_user.groups.build(group_params)
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
   end
 end
