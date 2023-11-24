9c584e1fed9de4de5796404800469e67ca64efc2
Alhassan Osman <oalhassan847@gmail.com>
2023-11-24 12:37:59 +0000

    permit profile photo in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_photo])
+    # For account update:
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:profile_photo])
+  end
 end
