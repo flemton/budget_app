49804f3218f4446d699046b32b39f67afd1122f9
Alhassan Osman <oalhassan847@gmail.com>
2023-11-25 10:46:31 +0000

    Style sign up screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[profile_photo name])
     # For account update:
     # devise_parameter_sanitizer.permit(:account_update, keys: [:profile_photo])
   end
