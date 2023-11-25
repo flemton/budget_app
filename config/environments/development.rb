9dbc324991733f4411c4f7920c334f251ffbb029
Alhassan Osman <oalhassan847@gmail.com>
2023-11-25 09:17:02 +0000

    Add icons

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,11 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.assets.compile = true
+
+  config.assets.configure do |env|
+    env.cache = ActiveSupport::Cache.lookup_store(:null_store)
+  end
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
