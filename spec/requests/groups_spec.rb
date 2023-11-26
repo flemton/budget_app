441565744da0762730421a2e43497a5d2552c1f3
Alhassan Osman <oalhassan847@gmail.com>
2023-11-26 00:41:51 +0000

    Update groups_spec.rb

--- spec/requests/groups_spec.rb
+++ spec/requests/groups_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'Groups', type: :request do
+  let(:user) do
+    User.create(name: 'User Name', email: 'user@gmail.com', password: 'password')
+  end
+  let(:group) { Group.create!(name: 'Test Group', icon: 'icon', user:) }
+
+  before do
+    sign_in user
+  end
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns http success' do
+      get groups_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      get group_path(group)
+      expect(response).to have_http_status(:success)
+    end
   end
 end
