d500eeacb2904d06d3a437b2b518c4e6ba1416cb
Alhassan Osman <oalhassan847@gmail.com>
2023-11-26 04:31:28 +0000

    add validations to entity

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -3,5 +3,6 @@ class Entity < ApplicationRecord
   belongs_to :group
 
   validates :name, presence: true
+  validates :name, length: { minimum: 2, maximum: 50 }
   validates :amount, presence: true, numericality: { greater_than: 0 }
 end
