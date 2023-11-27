7b559a6bd2f9ad81919085fef3309a7f7f7d6f9e
Alhassan Osman <oalhassan847@gmail.com>
2023-11-27 20:28:59 +0000

    Remove comment

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,30 +7,25 @@ RSpec.describe 'Users', type: :request do
         user: {
           email: 'user@example.com',
           password: 'password',
-          name: 'Test User' # Add or remove attributes based on your User model
+          name: 'Test User'
         }
       }
     end
-
     context 'with valid parameters' do
       it 'creates a new user and redirects' do
         expect do
           post user_registration_path, params: user_params
         end.to change(User, :count).by(1)
-
         expect(response).to have_http_status(:see_other)
       end
     end
-
     context 'with invalid parameters' do
       it 'does not create a user and renders the page again' do
         user_params[:user][:email] = 'invalid_email' # Making email invalid
-
         expect do
           post user_registration_path, params: user_params
         end.to_not change(User, :count)
-
-        expect(response).to have_http_status(:unprocessable_entity) # Or :success if it renders the same page
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
